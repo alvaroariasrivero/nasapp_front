075a9f4278cf06b779e06c2a30b4874366fc49fa
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-02-01 19:43:05 +0100

    axios get video worked

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,14 +7,22 @@ const Home = () => {
 
   useEffect(() => {
     async function fetchPicture(){
+      let image = {}
       try {
         const res = await axios.get('https://api.nasa.gov/planetary/apod?api_key=MosJdOel85j1l0s0tnx1YnKnzPIgsE568C47KbDs')
         const json = res.data
-        const image = {
+        if(json.media_type === 'video'){
+          image = {
+            'title': json.title,
+            'url': json.url,
+            'media_type': json.media_type
+          }
+        } else{
+        image = {
           'title': json.title,
           'hdurl': json.hdurl,
           'copyright': json.copyright
-        }
+        }}
       setpicture(image)
       } catch (error) {
         console.log('error', error)
@@ -22,11 +30,22 @@ const Home = () => {
     }
     fetchPicture();
   }, [])
-  return <div>
-    <img src={picture.hdurl} alt={picture.title} className='picture'/>
-    <p>Title: {picture.title}</p>
-    <p>Copyright: {picture.copyright}</p>
-  </div>;
+
+  if(picture.media_type){
+    return <div>
+            <div className="container">
+              <iframe src={picture.url} title={picture.title} className="video" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+            <p>Title: {picture.title}</p>
+            {/* <p>Copyright: {picture.copyright}</p> */}
+          </div>
+  }else{
+    return <div>
+            <img src={picture.hdurl} alt={picture.title} className='picture'/>
+            <p>Title: {picture.title}</p>
+            <p>Copyright: {picture.copyright}</p>
+          </div>
+  };
 };
 
 export default Home;
